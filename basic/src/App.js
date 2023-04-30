7fafd7688d1587396427b6f10cca6729115bab1d
Abhishek Shrivastav <abhishekshr7@gmail.com>
2023-04-30 23:18:57 +0530

    create component

--- basic/src/App.js
+++ basic/src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -16,7 +17,9 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+        <Home />
+        <Home />
       </header>
     </div>
   );
