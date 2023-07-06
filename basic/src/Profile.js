c0caf2971cd656377334126f325c8a5750c45041
Abhishek Shrivastav <abhishekshr7@gmail.com>
2023-07-06 21:33:29 +0530

    add show/hide in class component

--- basic/src/Profile.js
+++ basic/src/Profile.js
@@ -4,22 +4,25 @@ class Profile extends React.Component{
     constructor()
     {
         super();
-        //define state
         this.state = {
-            name: 'abhi',
-            email: 'abhi@mail.com' ,
-            count: 0
+            show:true
         }
+        //define state
+        // this.state = {
+        //     name: 'abhi',
+        //     email: 'abhi@mail.com' ,
+        //     count: 0
+        // }
 
     }
 
-    updateState()
-    {
-        this.setState({
-            name:'Bruce',
-            count: this.state.count+1
-        })
-    }
+    // updateState()
+    // {
+    //     this.setState({
+    //         name:'Bruce',
+    //         count: this.state.count+1
+    //     })
+    // }
 
     //state
     componentDidMount()
@@ -33,8 +36,13 @@ class Profile extends React.Component{
             <div style={{border: "solid white 4px"}}>
                 <h4>Profile CLASS Component</h4>
                 <p><b>Calling class props:-</b> {this.props.text.name} {this.props.data}</p>
-                <p><b>Calling class State:-</b> {this.state.name} {this.state.count} {this.state.email}</p>
-                <button onClick={()=>this.updateState()}>Update Name and count</button>
+                {/* <p><b>Calling class State:-</b> {this.state.name} {this.state.count} {this.state.email}</p>
+                <button onClick={()=>this.updateState()}>Update Name and count</button> */}
+                    {/* hide and show functionality */}
+                 {
+                    this.state.show ? <h5>Hide and show</h5> : null
+                }
+                <button onClick={()=>{this.setState({show:!this.state.show})}}>Click me</button>
             </div>
         )
     }
