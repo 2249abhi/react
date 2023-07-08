2e153f5e43a749530db5056eb5956569c549eed7
Abhishek Shrivastav <abhishekshr7@gmail.com>
2023-07-08 21:47:18 +0530

    create routes and call pages using link

--- basic/src/App.js
+++ basic/src/App.js
@@ -1,13 +1,26 @@
 //import logo from './logo.svg';
 import './App.css';
-import Home from './Home';
+import Layout from './cmp/Layout';
+import Home from './cmp/Home';
+import About from './cmp/About';
+import Contactus from './cmp/Contactus';
 import Profile from './Profile';
 import User from './User';
 import Forms from './Forms';
+import { BrowserRouter, Routes, Route, Outlet, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="contact" element={<Contactus />} />
+            <Route path="about" element={<About />} />
+          </Route>        
+        </Routes>
+      </BrowserRouter>
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         {/* <p>
@@ -21,7 +34,9 @@ function App() {
         >
           Learn React
         </a> */}
-        <Home />
+
+        
+        
         <Forms />
         <Profile text={{name:'ABHI'}} data="Profile class Data" />
         {/* <Profile text={{name:'rahul'}} data="rahul Profile class Data" /> */}
