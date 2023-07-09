7a236024f93c2b858451b3022406a5632b3ebb0e
Abhishek Shrivastav <abhishekshr7@gmail.com>
2023-07-09 22:38:53 +0530

    call api and show data in home page

--- basic/src/cmp/Home.js
+++ basic/src/cmp/Home.js
@@ -1,10 +1,46 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class Home extends Component {
+
+  constructor()
+  {
+    super();
+
+    this.state = {
+      users:null
+    }
+  }
+
+  componentDidMount()
+  {
+    fetch('https://reqres.in/api/users').then((resp)=>{
+      resp.json().then((result)=>{
+        //console.warn(result.data);
+        this.setState({users:result.data})
+
+      })
+    })
+  }
     
   render() {
     return (
-      <div>Home</div>
+      <div>
+        <p>Calling API in console (HOME Page)</p>
+        {
+          this.state.users ? 
+          this.state.users.map((item,i)=>
+            <div>
+              <p>
+                {i+1}. 
+                <b>First Name:</b> {item.first_name}, 
+                <b>Last Name:</b> {item.last_name},
+                <b>Email:</b> {item.email}
+              </p></div>
+          ) 
+          
+          : 'test'
+        }
+      </div>
     )
   }
 }
