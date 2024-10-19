1a3af88e92b1995953ef67348dcf8b468d14a86c
Abhishek Shrivastav <abhishekshr7@gmail.com>
2024-10-19 16:26:40 +0530

    added search parameter in profile page

--- first/src/FunctionalComponent/RoutingExample/Profile.jsx
+++ first/src/FunctionalComponent/RoutingExample/Profile.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, useSearchParams  } from 'react-router-dom'
 
 export default function Profile() {
-    let {name, dsg, salary} = useParams()
+    // let {name, dsg, salary} = useParams()
+    const [searchParams] = useSearchParams();
+    const name = searchParams.get('name');
+    const salary = searchParams.get('salary');
+    const dsg = searchParams.get('dsg');
   return (
     <>
     <h1>This Profile Component</h1>
     {name && <h2>Name: {name}</h2>}
-    {dsg && <h2>dsg: {dsg}</h2>}
+    {dsg && <h2>Designation: {dsg}</h2>}
     {salary && <h2>salary: {salary}</h2>}
     </>
   )
