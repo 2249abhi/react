f77f2e458426404fc99ec5d284d571cbca303110
Abhishek Shrivastav <abhishekshr7@gmail.com>
2024-10-19 17:58:57 +0530

    added bootstrap

--- first/src/FunctionalComponent/RoutingExample/Navbar.jsx
+++ first/src/FunctionalComponent/RoutingExample/Navbar.jsx
@@ -4,12 +4,30 @@ import { NavLink } from 'react-router-dom'
 export default function Navbar() {
   return (
     <>
-    <ul>
-        <li><NavLink to="">Home</NavLink></li>
-        <li><NavLink to="/about">About</NavLink></li>
-        <li><NavLink to="/profile">Profile</NavLink></li>
-        <li><NavLink to="/contact">Contact</NavLink></li>
-    </ul>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Abhisek</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <NavLink className="nav-link" to="">Home</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/about">About</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/profile">Profile</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/contact">Contact</NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>      
     </>
   )
 }
