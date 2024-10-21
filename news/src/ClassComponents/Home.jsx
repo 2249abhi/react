260cab7a39f086a2adee7bf3f2ebb6c9e1fbb394
Abhishek Shrivastav <abhishekshr7@gmail.com>
2024-10-21 23:32:22 +0530

    added package load more data on scroll

--- news/src/ClassComponents/Home.jsx
+++ news/src/ClassComponents/Home.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import InfiniteScroll from 'react-infinite-scroll-component';
 import NewsItem from './NewsItem'
 
 export default class Home extends Component {
@@ -6,12 +7,13 @@ export default class Home extends Component {
         super()
         this.state = {
             articles: [],
-            totalResults: 0
+            totalResults: 0,
+            page:1
         }
     }
 
-    async getAPIData() {
-        let response = await fetch(`https://newsapi.org/v2/everything?q=${this.props.search ? this.props.search : this.props.q}&from=2024-09-21&language=${this.props.language2}&sortBy=publishedAt&apiKey=13bcaa31eec24610aa23311ff5aa44fc`)
+    async getAPIData(page) {
+        let response = await fetch(`https://newsapi.org/v2/everything?q=${this.props.search ? this.props.search : this.props.q}&pagesize=12&page=${page}&from=2024-09-21&language=${this.props.language2}&sortBy=publishedAt&apiKey=13bcaa31eec24610aa23311ff5aa44fc`)
         response = await response.json()
         if (response.status === 'ok') {
             // this.setState({
@@ -19,62 +21,84 @@ export default class Home extends Component {
             //     totalResults: response.totalResults
             // })
 
-            const updatedArticles = await Promise.all(
-                response.articles
-                    .filter(x => x.title !== "[Removed]")
-                    .map(async (article) => {
-                        const imageExists = await this.checkImageExists(article.urlToImage);
-                        return {
-                            ...article,
-                            urlToImage: imageExists ? article.urlToImage : 'img/noimg.png',
-                        };
-                    })
-            );
+            // const updatedArticles = await Promise.all(
+            //     response.articles
+            //         .filter(x => x.title !== "[Removed]")
+            //         .map(async (article) => {
+            //             const imageExists = await this.checkImageExists(article.urlToImage);
+            //             return {
+            //                 ...article,
+            //                 urlToImage: imageExists ? article.urlToImage : 'img/noimg.png',
+            //             };
+            //         })
+            // );
 
-            this.setState({
-                articles: updatedArticles,
-                totalResults: response.totalResults,
-            });
+            if(page == 1) {
+                this.setState({
+                    articles: response.articles.filter(x=>x.title !== "[Removed]"),
+                    totalResults: response.totalResults
+                })
+            } else {
+                this.setState({
+                articles: this.state.articles.concat(response.articles.filter(x=>x.title !== "[Removed]"))
+            })
+            }
+            
         }
     }
 
     // Function to check if an image exists at a given URL
-    checkImageExists(url) {
-        return new Promise((resolve) => {
-            const img = new Image();
-            img.src = url;
-            img.onload = () => resolve(true); // Image exists
-            img.onerror = () => resolve(false); // Image doesn't exist or failed to load
-        });
-    }
+    // checkImageExists(url) {
+    //     return new Promise((resolve) => {
+    //         const img = new Image();
+    //         img.src = url;
+    //         img.onload = () => resolve(true); // Image exists
+    //         img.onerror = () => resolve(false); // Image doesn't exist or failed to load
+    //     });
+    // }
 
-    componentDidMount() {
-        this.getAPIData()
+    fetchData = ()=>{
+        this.getAPIData(this.state.page+1)
+        this.setState({page:this.state.page+1})
     }
-    componentDidUpdate(oldProps){
-        if(this.props !== oldProps) {
-            this.getAPIData()
+    componentDidMount() {
+        this.getAPIData(1)
+    }
+    componentDidUpdate(oldProps) {
+        if (this.props !== oldProps) {
+            this.getAPIData(1)
         }
     }
     render() {
         return (
             <>
-                <h5 className='background text-light text-center p-2 my-1'>{this.props.q} News Article</h5>
-                <div className='row'>
-                    {
-                        this.state.articles.map((item, index) => {
-                            return <NewsItem
-                                source={item.source?.name}
-                                title={item.title}
-                                description={item.description}
-                                image={item.urlToImage}
-                                url={item.url}
-                                published={item.publishedAt}
+                <h5 className='background text-light text-center p-2 my-1'>{this.props.search ? this.props.search : this.props.q} News Article</h5>
+                <InfiniteScroll
+                    dataLength={this.state.articles.length} //This is important field to render the next data
+                    next={this.fetchData}
+                    hasMore={this.state.articles.length < this.state.totalResults}
+                    loader={<div className='my-3 text-center'>
+                        <div className="spinner-border" role="status">
+                            <span className="visually-hidden">Loading...</span>
+                        </div>
+                    </div>}
+                >
+                    <div className='row'>
+                        {
+                            this.state.articles.map((item, index) => {
+                                return <NewsItem
+                                    source={item.source?.name}
+                                    title={item.title}
+                                    description={item.description}
+                                    image={item.urlToImage}
+                                    url={item.url}
+                                    published={item.publishedAt}
 
-                                key={index} />
-                        })
-                    }
-                </div>
+                                    key={index} />
+                            })
+                        }
+                    </div>
+                </InfiniteScroll>
             </>
         )
     }
--- news/src/ClassComponents/Navbar.jsx
+++ news/src/ClassComponents/Navbar.jsx
@@ -19,7 +19,7 @@ export default class Navbar extends Component {
             <>
                 <nav className="navbar navbar-expand-lg background sticky-top">
                     <div className="container-fluid">
-                        <Link className="navbar-brand text-light" to="">News</Link>
+                        <Link className="navbar-brand text-light" onClick={()=>this.props.changeSearch("")} to="">News</Link>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
@@ -28,14 +28,14 @@ export default class Navbar extends Component {
                                 <li className="nav-item">
                                     <Link className="nav-link text-light active" aria-current="page" to="/All">All</Link>
                                 </li>
-                                <li className="nav-item"><Link className="nav-link text-light" to="/Politics">Politics</Link></li>
-                                <li className="nav-item"><Link className="nav-link text-light" to="/Crime">Crime</Link></li>
-                                <li className="nav-item"><Link className="nav-link text-light" to="/Education">Education</Link></li>
-                                <li className="nav-item"><Link className="nav-link text-light" to="/Entertainment">Entertainment</Link></li>
-                                <li className="nav-item"><Link className="nav-link text-light" to="/Politics">Politics</Link></li>
-                                <li className="nav-item"><Link className="nav-link text-light" to="/Technology">Technology</Link></li>
-                                <li className="nav-item"><Link className="nav-link text-light" to="/Games">Games</Link></li>
-                                <li className="nav-item"><Link className="nav-link text-light" to="/Olympics">Olympics</Link></li>
+                                <li className="nav-item"><Link className="nav-link text-light" onClick={()=>this.props.changeSearch("")} to="/Politics">Politics</Link></li>
+                                <li className="nav-item"><Link className="nav-link text-light" onClick={()=>this.props.changeSearch("")} to="/Crime">Crime</Link></li>
+                                <li className="nav-item"><Link className="nav-link text-light" onClick={()=>this.props.changeSearch("")} to="/Education">Education</Link></li>
+                                <li className="nav-item"><Link className="nav-link text-light" onClick={()=>this.props.changeSearch("")} to="/Entertainment">Entertainment</Link></li>
+                                <li className="nav-item"><Link className="nav-link text-light" onClick={()=>this.props.changeSearch("")} to="/Politics">Politics</Link></li>
+                                <li className="nav-item"><Link className="nav-link text-light" onClick={()=>this.props.changeSearch("")} to="/Technology">Technology</Link></li>
+                                <li className="nav-item"><Link className="nav-link text-light" onClick={()=>this.props.changeSearch("")} to="/Games">Games</Link></li>
+                                <li className="nav-item"><Link className="nav-link text-light" onClick={()=>this.props.changeSearch("")} to="/Olympics">Olympics</Link></li>
                                 <li className="nav-item dropdown">
                                     <Link className="nav-link dropdown-toggle text-light text-decoration-underline" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                         Language
