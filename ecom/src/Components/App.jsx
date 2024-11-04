787135cfe964d5aa38fa314889bf2b8ebf8b42e5
Abhishek Shrivastav <abhishekshr7@gmail.com>
2024-11-04 21:40:04 +0530

    added theme

--- ecom/src/Components/App.jsx
+++ ecom/src/Components/App.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Navbar from './Partials/Navbar'
+import Footer from './Partials/Footer'
+import Home from './Home'
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+        <Navbar/>
+            <Routes>
+                <Route path='/' element={<Home/>} />
+            </Routes>
+        <Footer/>
+    </BrowserRouter>
   )
 }
